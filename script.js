a22033e6e0864f6d337ed3e6ea454702f51a15e9
EorlEli <51798919+EorlEli@users.noreply.github.com>
2021-11-10 14:33:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var principal=document.getElementById("principal").value;
+
+    /* making sure that the principle is positive*/
+    if(principal<=0)
+    {
+        alert('A principle should be a positive number')
+        return;
+    }  
+
+    /*caluclating the actual ineterest return*/
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+    result.innerHTML="If you deposit "+principal+" at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+" in the year "+year+"\<br\>"
 }
-        
+
+/* updating the interest*/
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
