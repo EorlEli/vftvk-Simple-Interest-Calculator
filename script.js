0423a38b426723c729de9b8fb374b58cdeee201a
EorlEli <51798919+EorlEli@users.noreply.github.com>
2021-11-11 08:04:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function compute()
     /* making sure that the principle is positive*/
     if(principal<=0)
     {
-        alert('A principle should be a positive number')
+        alert('A principle should be a positive number');
+        document.getElementById("principal").focus();
+	    document.getElementById("principal").value = "";
+	    document.getElementById("result").innerHTML= "";
         return;
     }  
 
@@ -16,12 +19,13 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
-    result.innerHTML="If you deposit "+principal+" at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+" in the year "+year+"\<br\>"
+    result.innerHTML="If you deposit <b><mark>"+ principal +"</mark></b>,\<br\>at an interest rate of <b><mark>"+ Number(document.getElementById("rate").value).toFixed(2) +" % </mark></b>,\<br\>You will receive an amount of <b><mark>" + interest +"</mark></b>,\<br\>in the year <b><mark>"+ year +"</mark></b>\<br\>";
 }
 
 /* updating the interest*/
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
+        
